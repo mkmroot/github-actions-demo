f3f012c5e21d17e99b8f2b83ebbdbb7f6b216e79
EC2 Default User <ec2-user@ip-172-31-21-6.ec2.internal>
2026-05-23 05:26:50 +0000

    edit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from AWS EC2! Did some Changes");
+  res.send("Hello from AWS EC2! Did some Changes!?");
 });
 
 app.listen(3000, () => {
